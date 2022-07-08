3f9e0a10614b04a46698fb2f3d0d81cb11c9f62d
ChristC9 <nyanhtetaungucsy106@gmail.com>
2022-07-08 16:47:11 +0630

    add image field db

--- base/models.py
+++ base/models.py
@@ -6,6 +6,7 @@ class Book(models.Model):
     title = models.CharField(max_length=255, null=True, blank=True)
     author = models.CharField(max_length=255, null=True, blank=True)
     link = models.CharField(max_length=255, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
     description = models.TextField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
